86a724ae6d3be30138009a5337bb803a34d09744
Aleksandr Prykhodko <aleksandr.prykhodko@akumyn.com.au>
2021-09-26 21:36:23 +1000

    fixed first moq Service

--- FlyingDutchmanAirlines/ServiceLayer/BookingService.cs
+++ FlyingDutchmanAirlines/ServiceLayer/BookingService.cs
@@ -11,41 +11,34 @@ namespace FlyingDutchmanAirlines.ServiceLayer
         private readonly BookingRepository _bookingRepository;
         private readonly CustomerRepository _customerRepository;
 
-        //public BookingService(BookingRepository bookingRepository)
-        //{
-        //    _bookingRepository = bookingRepository;
-        //}
-
-        public BookingService(BookingRepository bookingRepository,
-            CustomerRepository customerRepository)
+        public BookingService(BookingRepository bookingRepository, CustomerRepository customerRepository)
         {
             _bookingRepository = bookingRepository;
             _customerRepository = customerRepository;
         }
 
-        public async Task<(bool, Exception)> CreateBooking(string customerName,
-            int flightNumber)
+        public async Task<(bool, Exception)> CreateBooking(string name, int flightNumber)
         {
             try
             {
                 Customer customer;
                 try
                 {
-                    customer = await _customerRepository.GetCustomerByName(customerName);
+                    customer = await _customerRepository.GetCustomerByName(name);
                 }
-                catch (CustomerNotFoundException)
+                catch (FlightNotFoundException)
                 {
-                    await _customerRepository.CreateCustomer(customerName);
-                    return await CreateBooking(customerName, flightNumber);
+                    await _customerRepository.CreateCustomer(name);
+                    return await CreateBooking(name, flightNumber);
                 }
+
                 await _bookingRepository.CreateBooking(customer.CustomerId, flightNumber);
                 return (true, null);
             }
-            catch (Exception ex)
+            catch (Exception exception)
             {
-                return (false, ex);
+                return (false, exception);
             }
         }
     }
-
 }
